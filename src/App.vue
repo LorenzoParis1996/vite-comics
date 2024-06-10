2ecb13725afc1ff3cd4708b8f4e60ffe3f038238
LorezoParis1996 <lorenzoparis86@live.it>
2024-06-10 15:21:31 +0200

    creato progetto vite con readme esercizio

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+
+</style>
