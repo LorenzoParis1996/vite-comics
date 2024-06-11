7c7ca0a8e0dbee0f6864af535848f8e11d5d62cf
LorezoParis1996 <lorenzoparis86@live.it>
2024-06-11 14:53:32 +0200

    aggiunto jumbo al component main e copiato file json

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,6 +7,9 @@ export default {
 </script>
 
 <template>
+  <div class="jumbo-main">
+     
+  </div>
   <div class="content">
    <h1>--> Content goes here <--</h1>
   </div>
@@ -15,7 +18,12 @@ export default {
 <style lang="scss" scoped>
 @use '../styles/general.scss';
 
-  .content {
+.jumbo-main {
+   background-image: url(../img/jumbotron.jpg);
+   padding: 13rem;
+}
+
+.content {
     background-color: #1c1c1c;
     color: white;
     padding: 3rem;
@@ -24,5 +32,5 @@ export default {
     h1 {
       padding-left: 13rem;
     }
-  }
+ }
 </style>
